a19e6acf352b9058ba8efbb9ac74caca753bef86
Eduard-Kalenkovich <125797635+Eduard-Kalenkovich@users.noreply.github.com>
2023-03-09 19:19:30 +0300

    Add files via upload

---
+++ dz1.cs
@@ -0,0 +1,12 @@
+﻿static bool IsPalindrome(string s)
+{
+    for (int i = 0; i < s.Length / 2; ++i)
+        if (s[i] != s[s.Length - 1 - i]) 
+        return false;
+    return true;
+}
+Console.WriteLine("Введите данные");
+if (IsPalindrome(Console.ReadLine()))
+Console.WriteLine("Полиндром");
+else
+Console.WriteLine("Не полиндром");
